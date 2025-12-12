eb01f3d379dc9bee74f6d30904a21aa7a3e6d18f
tballoon23 <asbssu18@gmail.com>
2025-12-12 19:02:20 +0900

    fix : edit bat, proj #39

--- TeamProject/UpdateLib.bat
+++ TeamProject/UpdateLib.bat
@@ -3,6 +3,15 @@ setlocal
 
 set BASE=%~dp0
 
+set CONFIG=%~1
+if /I "%CONFIG%" == "Debug" (
+    set "PHYSX_DIR=Engine\ThirdPartyLib\PhysX\deb"
+    echo [UpdateLib] Mode: DEBUG (Source: Engine\ThirdPartyLib\PhysX\deb)
+) else (
+    set "PHYSX_DIR=Engine\ThirdPartyLib\PhysX\rel"
+    echo [UpdateLib] Mode: RELEASE (Source: Engine\ThirdPartyLib\PhysX\rel)
+)
+
 REM 생성될 때까지 기다리는 함수
 call :WaitForFile "%BASE%Engine\Bin\Engine.dll" 5
 call :WaitForFile "%BASE%Engine\Bin\Engine.lib" 5
@@ -14,7 +23,7 @@ call :WaitForFile "%BASE%Engine\ThirdPartyLib\fmod_vc.lib" 1
 call :WaitForFile "%BASE%Engine\ThirdPartyLib\fmodL_vc.lib" 1
 
 REM PhysX
-call :WaitForFile "%BASE%Engine\Bin\PhysX_64.lib" 1
+call :WaitForFile "%BASE%%PHYSX_DIR%\PhysX_64.lib" 1
 
 
 REM 여기서 부터 자신 프로젝트 기준으로 복사====================DemoProject부분을 자신 폴더명으로
@@ -24,10 +33,11 @@ xcopy /y "%BASE%Engine\Bin\Engine.dll"                                          
 xcopy /y "%BASE%Engine\ThirdPartyLib\fmodL.dll"                         "%BASE%DemoProject\Bin\"
 xcopy /y "%BASE%Engine\ThirdPartyLib\fmod.dll"                          "%BASE%DemoProject\Bin\"
 
-xcopy /y "%BASE%Engine\Bin\PhysX_64.dll"           "%BASE%DemoProject\Bin\"
-xcopy /y "%BASE%Engine\Bin\PhysXCommon_64.dll"     "%BASE%DemoProject\Bin\"
-xcopy /y "%BASE%Engine\Bin\PhysXFoundation_64.dll" "%BASE%DemoProject\Bin\"
-xcopy /y "%BASE%Engine\Bin\PhysXCooking_64.dll" "%BASE%DemoProject\Bin\"
+xcopy /y "%BASE%%PHYSX_DIR%\PhysX_64.dll"           "%BASE%DemoProject\Bin\"
+xcopy /y "%BASE%%PHYSX_DIR%\PhysXCommon_64.dll"     "%BASE%DemoProject\Bin\"
+xcopy /y "%BASE%%PHYSX_DIR%\PhysXFoundation_64.dll" "%BASE%DemoProject\Bin\"
+xcopy /y "%BASE%%PHYSX_DIR%\PhysXCooking_64.dll"    "%BASE%DemoProject\Bin\"
+xcopy /y "%BASE%%PHYSX_DIR%\PhysXGpu_64.dll"        "%BASE%DemoProject\Bin\"
 
 xcopy /y /s /e /i "%BASE%Engine\Bin\Engine_Shaders\"                "%BASE%DemoProject\Bin\ShaderFiles\"
 xcopy /y /s /e /i "%BASE%Engine\Public\Engine_Shader\"          "%BASE%DemoProject\Bin\ShaderFiles\"
@@ -44,13 +54,17 @@ xcopy /y "%BASE%Engine\ThirdPartyLib\assimp-vc143-mt.lib"           "%BASE%Engin
 xcopy /y "%BASE%Engine\ThirdPartyLib\DirectXTKd.lib"                        "%BASE%EngineSDK\Lib\"
 xcopy /y "%BASE%Engine\ThirdPartyLib\DirectXTK.lib"                           "%BASE%EngineSDK\Lib\"
 
-xcopy /y "%BASE%Engine\ThirdPartyLib\PhysX_64.lib"                                                "%BASE%EngineSDK\Lib\"
-xcopy /y "%BASE%Engine\ThirdPartyLib\PhysXCommon_64.lib"                              "%BASE%EngineSDK\Lib\"
-xcopy /y "%BASE%Engine\ThirdPartyLib\PhysXFoundation_64.lib"                         "%BASE%EngineSDK\Lib\"
-xcopy /y "%BASE%Engine\ThirdPartyLib\PhysXExtensions_static_64.lib"            "%BASE%EngineSDK\Lib\"
-xcopy /y "%BASE%Engine\ThirdPartyLib\PhysXPvdSDK_static_64.lib"                           "%BASE%EngineSDK\Lib\"
-xcopy /y "%BASE%Engine\ThirdPartyLib\PhysXCharacterKinematic_static_64.lib"   "%BASE%EngineSDK\Lib\"
-xcopy /y "%BASE%Engine\ThirdPartyLib\PhysXCooking_64.lib"                                         "%BASE%EngineSDK\Lib\"
+xcopy /y "%BASE%%PHYSX_DIR%\PhysX_64.lib"                      "%BASE%EngineSDK\Lib\"
+xcopy /y "%BASE%%PHYSX_DIR%\PhysXCommon_64.lib"                 "%BASE%EngineSDK\Lib\"
+xcopy /y "%BASE%%PHYSX_DIR%\PhysXFoundation_64.lib"             "%BASE%EngineSDK\Lib\"
+xcopy /y "%BASE%%PHYSX_DIR%\PhysXExtensions_static_64.lib"      "%BASE%EngineSDK\Lib\"
+xcopy /y "%BASE%%PHYSX_DIR%\PhysXPvdSDK_static_64.lib"          "%BASE%EngineSDK\Lib\"
+xcopy /y "%BASE%%PHYSX_DIR%\PhysXCharacterKinematic_static_64.lib" "%BASE%EngineSDK\Lib\"
+xcopy /y "%BASE%%PHYSX_DIR%\PhysXCooking_64.lib"                "%BASE%EngineSDK\Lib\"
+if exist "%BASE%%PHYSX_DIR%\*.pdb" (
+    xcopy /y "%BASE%%PHYSX_DIR%\*.pdb" "%BASE%EngineSDK\Lib\"
+)
+
 
 goto :eof
 
