d8b4fb1bfdbcbc957081c9bb8df8edb96b84ccb0
na4742 <126836684+na4742@users.noreply.github.com>
2025-12-14 10:17:06 +0900

    feat : 애니매이션 그래프 및 클립 데이터구조 확립 #29

--- TeamProject/AnimationTool/Public/AnimationLayout.h
+++ TeamProject/AnimationTool/Public/AnimationLayout.h
@@ -3,40 +3,55 @@
 
 enum class ANIM_LAYER_STATE { OVERRIDE, INTERRUPT, ADDITIVE };
 enum class ANIM_BLEND_STATE {};
+enum class ANIM_EVENT_STATE { EFFECT, SOUND };
 
+//NLOHMANN_JSON_SERIALIZE_ENUM(ANIM_EVENT_STATE, {
+//    {ANIM_EVENT_STATE::Sound, "Sound"},
+//    {ANIM_EVENT_STATE::Effect, "Effect"}
+//}) 
 
+typedef struct AnimationEvent {
+    float   fEventTime;     //이벤트 작동 시간
+    int     iEventState;    //ANIM_EVENT_STATE
+    string  EventTag;       //이벤트 태그 
+}ANIM_EVENT;
 
+typedef struct AnimationClip {
+    string ClipTag; 
+    vector<ANIM_EVENT> Events;
+}ANIM_CLIP;
 
 typedef struct AnimationNode {
-    string  NodeTag;    //노드 이름
-    _int    ClipIndex;  //클립 인덱스
-    _float  PlaySpeed;  //재생속도 (Playback Speed)
-    _bool   isLoop;     //반복여부
+    string NodeTag;     //노드 이름
+    int    iClipIndex;  //클립 인덱스
+    float  fPlaySpeed;  //재생속도
+    bool   bisLoop;     //반복여부
 }ANIM_NODE;
 
 typedef struct AnimationTransition { // to와 from을 둘 다 충족시킬시 변경
     string toNode;      //현재 재생중인 노드
     string fromNode;    //에서 바꿀노드
+    int    iBlendState;  //ANIM_BLEND_STATE
 }ANIM_TRANSIT;
 
 typedef struct AnimationLayer {
     //레이어 데이터
-    string  LayerTag;                                          // 레이어 이름
-    _int    LayerIndex = { -1 };                               // 레이어 인덱스 (낮을수록 베이스)
-    _uint   LayerType = { ENUM(ANIM_LAYER_STATE::OVERRIDE) };  // 레이어 타입
-    _int    StartBoneIndex = { -1 };                           // 어느 본부터 적용해 내려갈지
-    _uint   DefaultNodeIndex = { 0 };                          // 기본 노드
+    string  LayerTag;                                        // 레이어 이름
+    int   iLayerIndex = { -1 };                              // 레이어 인덱스 (낮을수록 베이스)
+    int   iLayerType = { ENUM(ANIM_LAYER_STATE::OVERRIDE) };  // 레이어 타입
+    int   iStartBoneIndex = { -1 };                          // 어느 본부터 적용해 내려갈지
+    int   iDefaultNodeIndex = { 0 };                          // 기본 노드
 
     //노드 데이터
-    _int NumNodes;                //노드 개수
-    vector<ANIM_NODE>  mAnimNode; //노드 데이터
-    _int NumTransition;           //트랜지션 개수
-    vector<ANIM_NODE>  mAnimNode; //트랜지션 데이터
+    int iNumNodes;                      //노드 개수
+    vector<ANIM_NODE> AnimNode;         //노드 데이터
+    int iNumTransition;                 //트랜지션 개수
+    vector<ANIM_TRANSIT> TransitNode;   //트랜지션 데이터
 }ANIM_LAYER;
 
 
 typedef struct AnimationGraph {
-    string              GraphTag;   //파일이름
-    _uint               NumLayers;  //레이어 개수
-    vector<ANIM_LAYER>  Layers;     //레이어 데이터
+    string              GraphTag;           //파일이름
+    int                 NumLayers = { 0 };  //레이어 개수
+    vector<ANIM_LAYER>  Layers;             //레이어 데이터
 }ANIM_GRAPH;
