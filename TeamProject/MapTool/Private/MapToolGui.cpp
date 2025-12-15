0d42d6b111fc7dde1a019020a5e4b05d263be6b5
yugi0714 <45875223+yugi0714@users.noreply.github.com>
2025-12-15 15:15:17 +0900

    feat : RayPicking해서 오브젝트 선택하고 삭제하는 기능 추가 #88

--- TeamProject/MapTool/Private/MapToolGui.cpp
+++ TeamProject/MapTool/Private/MapToolGui.cpp
@@ -2,6 +2,9 @@
 #include "MapToolGui.h"
 #include "GameInstance.h"
 
+#include "StaticObject.h"
+#include "Layer.h"
+#include "RayReceiver.h"
 
 CMapToolGui::CMapToolGui(GUI_CONTEXT* pContext)
 	: CBasePanel(pContext)
@@ -20,12 +23,9 @@ HRESULT CMapToolGui::Initialize()
 void CMapToolGui::Update_Panel(_float dt)
 {
     Compute_Ray();
+    KeyInput();
 
-	if (m_pGameInstance->Get_InputDev()->Key_Tap('P')) {
-		RAY_HIT* pRayHit = m_pGameInstance->Get_RayMgr()->Get_FrontRayHit();
-		m_vRayHitPos = pRayHit->vHittedPosition;
-        Place_Object(pRayHit);
-	}
+
 }
 
 void CMapToolGui::Render_GUI()
@@ -39,6 +39,15 @@ void CMapToolGui::Render_GUI()
     ImGui::TextColored(ImVec4(1.f, 1.f, 1.f, 1.f), "Scale");
     ImGui::InputFloat3("##Scale", reinterpret_cast<float*>(&m_vScale_PlacedObject), "%.1f");
     
+    ImGui::Text("");
+    if (ImGui::Checkbox("IsObjectPicking", &m_isObjectPicking)) {
+
+        Set_ObjectPicking(m_isObjectPicking);
+
+    }
+
+    
+
 
 	ImGui::PopID();
 }
@@ -109,14 +118,73 @@ void CMapToolGui::Place_Object(RAY_HIT* pRayHit)
 
     IObjectService* pObjMgr = m_pGameInstance->Get_ObjectMgr();
 
+    CStaticObject::STATIC_OBJECT_DESC* Desc = new CStaticObject::STATIC_OBJECT_DESC;
+    Desc->isRayReceiver = m_isObjectPicking;
+
     CGameObject* pStaticObject = Builder::Create_Object({ "MapTool_Level" ,"Proto_GameObject_StaticObject" })
     	.Position(pRayHit->vHittedPosition)
     	.Scale(m_vScale_PlacedObject)
+        .Add_ObjDesc(Desc)
     	.Build("Static_Model");
     
     pObjMgr->Add_Object(pStaticObject,	{ "MapTool_Level", "Static_Layer" });
+}
 
+void CMapToolGui::Set_ObjectPicking(_bool is)
+{
+    CLayer* pStaticLayer = m_pGameInstance->Get_ObjectMgr()->Get_Layer({ "MapTool_Level", "Static_Layer" });
+    if (nullptr == pStaticLayer)
+        return;
 
+    for (auto& pObject : pStaticLayer->Get_AllObject()) {
+        pObject->Get_Component<CRayReceiver>()->Set_CompActive(is);
+    }
+}
+
+void CMapToolGui::KeyInput()
+{
+    auto pInputDev = m_pGameInstance->Get_InputDev();
+
+    // 오브젝트 피킹
+    if (pInputDev->Mouse_Tap(MOUSE_BTN::LB)) {
+        // 오브젝트 피킹이 꺼져있을때 임시로 킴
+        _bool isResetObjectPicking = false;
+        if (false == m_isObjectPicking) {
+            isResetObjectPicking = true;
+            Set_ObjectPicking(true);
+        }
+        
+        RAY_HIT* pRayHit = m_pGameInstance->Get_RayMgr()->Get_FrontRayHit();
+        if (nullptr != pRayHit) {
+            if ("Static_Layer" == pRayHit->pObject->Get_Layer()->Get_LayerTag())
+                CGameInstance::GetInstance()->Get_GUISystem()->Get_Context()->pSelectedObject = pRayHit->pObject;
+        }
+
+        if (true == isResetObjectPicking)
+            Set_ObjectPicking(false);
+    }
+
+    // Inspector 창에 떠있는 오브젝트 삭제
+    if (pInputDev->Key_Tap(VK_DELETE)) {
+        auto pGuiContext = m_pGameInstance->Get_GUISystem()->Get_Context();
+
+        if (nullptr != pGuiContext->pSelectedObject && 
+            "Static_Layer" == pGuiContext->pSelectedObject->Get_LayerDesc().LayerTag &&
+            nullptr != dynamic_cast<CStaticObject*>(pGuiContext->pSelectedObject)) {
+
+            static_cast<CStaticObject*>(pGuiContext->pSelectedObject)->Delete_Object();
+
+        }
+    }
+
+    // 레이피킹으로 오브젝트 배치
+    if (m_pGameInstance->Get_InputDev()->Key_Tap('P')) {
+        RAY_HIT* pRayHit = m_pGameInstance->Get_RayMgr()->Get_FrontRayHit();
+        if (nullptr != pRayHit) {
+            m_vRayHitPos = pRayHit->vHittedPosition;
+            Place_Object(pRayHit);
+        }
+    }
 }
 
 CMapToolGui* CMapToolGui::Create(GUI_CONTEXT* pContext)
