66d37c06069f190518eb05d79972293554d85073
na4742 <126836684+na4742@users.noreply.github.com>
2025-12-16 12:18:51 +0900

    feat : 테스트용 모델 생성 #101

--- TeamProject/AnimationTool/Private/Grid.cpp
+++ TeamProject/AnimationTool/Private/Grid.cpp
@@ -50,6 +50,9 @@ HRESULT CGrid::Initialize(INIT_DESC* pArg)
 		MaterialDat->Link_Shader(G_GlobalLevelKey, "VTX_PlaneGrid.hlsl");
 
 	customInstance->Get_MaterialData()->Link_Texture(G_GlobalLevelKey, "TileCell.png", TEXTURE_TYPE::DIFFUSE);
+
+	Get_Component<CTransform>()->Scale(_float3(100.f, 100.f, 100.f));
+
 	return S_OK;
 }
 
