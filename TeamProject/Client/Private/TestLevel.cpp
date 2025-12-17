10d94b5c5bb4cb101fc547f10650faf77408eb57
yugi0714 <45875223+yugi0714@users.noreply.github.com>
2025-12-18 01:24:30 +0900

    feat : Client MapData Load 추가 #159

--- TeamProject/Client/Private/TestLevel.cpp
+++ TeamProject/Client/Private/TestLevel.cpp
@@ -7,6 +7,10 @@
 
 #include "Camera.h"
 
+/* MapData */
+#include "MapLoader.h"
+#include "MapPlacedObject.h"
+
 CTestLevel::CTestLevel(const string& LevelKey)
 	:CLevel(LevelKey),
 	m_pGameInstance{ CGameInstance::GetInstance() }
@@ -16,6 +20,9 @@ CTestLevel::CTestLevel(const string& LevelKey)
 
 HRESULT CTestLevel::Initialize()
 {
+
+
+
 	return S_OK;
 }
 
@@ -25,7 +32,17 @@ HRESULT CTestLevel::Awake()
 
 	pProto->Add_ProtoType("Test_Level", "Proto_GameObject_FreeCam", CFreeCam::Create());
 	pProto->Add_ProtoType("Test_Level", "Proto_GameObject_TestModel", CTestObject::Create());
+	pProto->Add_ProtoType("Test_Level", "Proto_GameObject_MapPlacedObject", CMapPlacedObject::Create());
 
+	// Ready MapObject key and path to ResourceMgr 
+	Rake_MapResources();
+
+	//경로는 임시. 추후 로직 변경예정
+	CMapLoader* pMapLoader = CMapLoader::Create("Test_Level",  "../Bin/Resources/MapData/Data/MapTool.Data_1_20251217_200942.json" );
+	if (nullptr == pMapLoader)
+		MSG_BOX("Failed to Load MapData!");
+	// 바로 지움
+	Safe_Release(pMapLoader);
 
 	Ready_Camera();
 	return S_OK;
@@ -46,6 +63,25 @@ void CTestLevel::Ready_Camera()
 
 	pObjMgr->Add_Object(Camera, { "Test_Level","Camera_Layer" });
 	m_pGameInstance->Get_CameraMgr()->Set_MainCam(Camera->Get_Component<CCamera>());
+}
+
+void CTestLevel::Rake_MapResources()
+{
+	auto pRcsMgr = CGameInstance::GetInstance()->Get_ResourceMgr();
+	for (const auto& entry : filesystem::recursive_directory_iterator("../Bin/Resources/MapData/Model/"))
+	{
+		if (entry.is_regular_file() && entry.path().extension() == ".model")
+		{
+			filesystem::path ModelPath = entry.path();
+			filesystem::path MaterialPath = ModelPath;
+			MaterialPath.replace_extension(".mat");
+
+
+			pRcsMgr->Add_ResourcePath(ModelPath.filename().string(), ModelPath.string());
+			pRcsMgr->Add_ResourcePath(MaterialPath.filename().string(), MaterialPath.string());
+
+		}
+	}
 }
 
 HRESULT CTestLevel::Render()
