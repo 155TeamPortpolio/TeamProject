8f73fc626a45b43694ce252ab32fd8825d57c070
tballoon23 <asbssu18@gmail.com>
2025-12-22 22:20:07 +0900

    refactor : 조건 분기 단순화 #207

--- TeamProject/Client/Private/TestObject.cpp
+++ TeamProject/Client/Private/TestObject.cpp
@@ -83,29 +83,22 @@ HRESULT CTestObject::Initialize_State()
 	m_pStateMachine->Register_Transition("Walk", "Idle",
 		CTestStateMachine::CONDITION_BOOL_FALSE, "IsMoving");
 
-	// AnyState -> Jump (점프 키 입력)
+	// AnyState -> Jump
 	m_pStateMachine->Register_AnyStateTransition("Jump",
 		CTestStateMachine::CONDITION_TRIGGER, "Jump");
 
-	// AnyState -> Dash (Shift 입력)
+	// AnyState -> Dash
 	m_pStateMachine->Register_AnyStateTransition("Dash",
 		CTestStateMachine::CONDITION_TRIGGER, "Dash");
 
-	// Jump -> Walk (착지 + 이동 중)
-	m_pStateMachine->Register_Transition("Jump", "Walk",
-		CTestStateMachine::CONDITION_BOOL_TRUE, "IsGroundedAndMoving");
-
-	// Jump -> Idle (착지 + 정지)
+	// Jump -> Idle (착지하면 무조건 Idle로)
 	m_pStateMachine->Register_Transition("Jump", "Idle",
-		CTestStateMachine::CONDITION_BOOL_TRUE, "IsGroundedAndIdle");
+		CTestStateMachine::CONDITION_BOOL_TRUE, "IsGrounded");
 
-	// Dash -> Walk (대쉬 완료 + 이동 중)  
-	m_pStateMachine->Register_Transition("Dash", "Walk",
-		CTestStateMachine::CONDITION_BOOL_TRUE, "DashFinishedAndMoving");
-
-	// Dash -> Idle (대쉬 완료 + 정지) 
+	// Dash -> Idle (대쉬 완료되면 무조건 Idle로)
 	m_pStateMachine->Register_Transition("Dash", "Idle",
-		CTestStateMachine::CONDITION_BOOL_TRUE, "DashFinishedAndIdle");
+		CTestStateMachine::CONDITION_BOOL_TRUE, "DashFinished");
+
 
 	// 기본 상태 설정
 	m_pStateMachine->Set_DefaultState("Idle");
@@ -149,18 +142,8 @@ void CTestObject::Update(_float dt)
 	auto pCCT = Get_Component<CCharacterController>();
 	if (pCCT)
 	{
-		_bool bGrounded = pCCT->Is_Grounded();
-		_bool bMoving = m_vInputDir.Length() > 0.01f;
-		_bool bDashFinished = m_pStateMachine->Get_Bool("DashFinished");
-
-		m_pStateMachine->Set_Bool("IsGrounded", bGrounded);
-		m_pStateMachine->Set_Bool("IsMoving", bMoving);
-
-		m_pStateMachine->Set_Bool("IsGroundedAndMoving", bGrounded && bMoving);
-		m_pStateMachine->Set_Bool("IsGroundedAndIdle", bGrounded && !bMoving);
-
-		m_pStateMachine->Set_Bool("DashFinishedAndMoving", bDashFinished && bMoving);
-		m_pStateMachine->Set_Bool("DashFinishedAndIdle", bDashFinished && !bMoving);
+		m_pStateMachine->Set_Bool("IsGrounded", pCCT->Is_Grounded());
+		m_pStateMachine->Set_Bool("IsMoving", m_vInputDir.Length() > 0.01f);
 	}
 
 	if (m_bJump)
