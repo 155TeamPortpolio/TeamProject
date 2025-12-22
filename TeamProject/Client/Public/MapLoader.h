a5c472689b8e63f8487da1db494ec3b3c1ed165f
yugi0714 <45875223+yugi0714@users.noreply.github.com>
2025-12-22 11:16:40 +0900

    fix : MapLoader 슬롯데이터 컨테이너 변수명 변경 #226

--- TeamProject/Client/Public/MapLoader.h
+++ TeamProject/Client/Public/MapLoader.h
@@ -10,8 +10,8 @@ public:
 	enum class MAPOBJ_TYPE {PLACED, FLOOR, TRIGGER, END};
 
 	using ObjFields			= vector<FIELD_DATA>;								// 한 데이터 묶음(ObjID, 변수명, 값)
-	using ObjFieldMap		= unordered_map<int32_t, ObjFields>;				// ObjID별로 매핑된 데이터 묶음
-	using SlotFormatData	= unordered_map<std::string, ObjFieldMap>;	// slotFormat별로 매핑된 데이터 묶음
+	using ObjFieldMap		= unordered_map<int, ObjFields>;				// ObjID별로 매핑된 데이터 묶음
+	using SlotFormatData	= unordered_map<string, ObjFieldMap>;	// slotFormat별로 매핑된 데이터 묶음
 
 private:
 	CMapLoader();
@@ -34,7 +34,10 @@ private:
 	vector<string>	m_TagLayers;
 
 	MapData_Header	m_MapBaseData = {};
-	SlotFormatData	m_SlotFormatData = {};
+	//SlotFormatData	m_SlotFormatData = {};
+	/* Format -> ObjID -> Data
+	unordered_map<Format유형(effect, physics...), unordered_map<오브젝트 ID, vector<(ObjID, 변수명, 값)>>>*/
+	unordered_map<string, unordered_map<_int, vector<FIELD_DATA>>>	m_SlotFormatData;
 
 	_bool			m_hasColliderData = {};
 
