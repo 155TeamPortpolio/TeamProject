f47ba54a5b835b46d32d3224eda69a2793588d65
조혜연 <adencho1204@naver.com>
2025-12-23 17:41:46 +0900

    fix: lerp

--- TeamProject/EngineSDK/Inc/Engine_Shader/Shader_Function.hlsl
+++ TeamProject/EngineSDK/Inc/Engine_Shader/Shader_Function.hlsl
@@ -17,7 +17,7 @@ float2 CalculateFrameIndex(uint Col, uint Row, uint FrameIndex, float2 InTexcoor
 //Thereshold - 어느정도 밝기 이상 추출할건지 (높을수록 진짜 밝은것만 낮을수록 더 많이)
 //Softness - 부드러운정도
 //Intensity - 밝기
-float4 ExtractBright(float4 color, float Thereshold =1.f, float Softness = 0.5f, float Intensity = 1.f)
+float4 ExtractBright(float4 color, float Thereshold =2.f, float Softness = 0.5f, float Intensity = 1.f)
 {
     float Bright = dot(color.rgb, float3(0.2126, 0.7152, 0.0722));
     
