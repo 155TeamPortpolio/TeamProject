34e4d373fce5f6be9bdd3db5c22e408e49b7fbd2
jehyun1108 <andixuan_1108@naver.com>
2025-12-23 16:57:54 +0900

    feat : Camera 코드 리팩토링 #255

--- TeamProject/CameraTool/private/CamObj.cpp
+++ TeamProject/CameraTool/private/CamObj.cpp
@@ -5,24 +5,25 @@
 HRESULT CCamObj::Initialize_Prototype()
 {
 	__super::Initialize_Prototype();
-	cam = Add_Component<CCamera>();
-	light = Add_Component<CLight>();
+	Add_Component<CCamera>();
+	Add_Component<CLight>();
 	Add_Component<CCamSequencePlayer>();
-	game = CGameInstance::GetInstance();
 	return S_OK;
 }
 
 HRESULT CCamObj::Initialize(INIT_DESC* arg)
 {
 	__super::Initialize(arg);
+
 	LIGHT_DESC desc{};
-	desc.vLightPosition  = {};
-	desc.fLightRange     = {};
-	desc.vLightDirection = { 0.0f, -1.0f, 1.0f, 0.0f };
-	desc.vLightDiffuse   = { 1.0f,  1.0f, 1.0f, 1.0f };
-	desc.vLightAmbient   = { 0.6f,  0.6f, 0.6f, 1.0f };
-	desc.vLightSpecular  = { 1.0f,  1.0f, 1.0f, 1.0f };
-	light->Set_Desc(desc, LIGHT_TYPE::DIRECTIONAL);
+	desc.vLightPosition = {};
+	desc.fLightRange = {};
+	desc.fLightIntensity = 3.f;
+	desc.vLightDirection = {0.0f, -1.0f, 1.0f, 0.0f};
+	desc.vLightDiffuse = {1.0f, 1.0f, 1.0f, 1.0f};
+	desc.vLightAmbient = {0.6f, 0.6f, 0.6f, 1.0f};
+	desc.vLightSpecular = {1.0f, 1.0f, 1.0f, 1.0f};
+	Get_Component<CLight>()->Set_Desc(desc, LIGHT_TYPE::DIRECTIONAL);
 
 	return S_OK;
 }
