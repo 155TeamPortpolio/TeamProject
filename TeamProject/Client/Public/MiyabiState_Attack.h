e113e11184c14281af326b90eb40364b6d95c547
tballoon23 <asbssu18@gmail.com>
2025-12-24 11:22:30 +0900

    feat : 공격 종료 모션 추가 #207

--- TeamProject/Client/Public/MiyabiState_Attack.h
+++ TeamProject/Client/Public/MiyabiState_Attack.h
@@ -13,6 +13,13 @@ public:
     virtual void Exit(CMiyabi* pOwner) override;
 
 public:
+    void    Set_AttackIndex(_uint iAttack) { m_iAttackIdx = iAttack; }
+    _uint   Get_AttackIndex() const { return m_iAttackIdx; }
+
+private:
+    _uint   m_iAttackIdx;
+
+public:
     static CMiyabiState_Attack* Create() { return new CMiyabiState_Attack(); }
     virtual void Free() override { __super::Free(); }
 };
@@ -88,6 +95,18 @@ public:
 
 public:
     static CMiyabiState_Attack_06* Create() { return new CMiyabiState_Attack_06(); }
+    virtual void Free() override { __super::Free(); }
+};
+
+class CMiyabiState_Attack_End : public IBaseState<CMiyabi>
+{
+public:
+    virtual void Enter(CMiyabi* pOwner) override;
+    virtual void Update(CMiyabi* pOwner, _float dt) override;
+    virtual void Exit(CMiyabi* pOwner) override;
+
+public:
+    static CMiyabiState_Attack_End* Create() { return new CMiyabiState_Attack_End(); }
     virtual void Free() override { __super::Free(); }
 };
 NS_END
