e9a68f032bb85b30f2c8b8e0cb31defb09882250
jehyun1108 <andixuan_1108@naver.com>
2025-12-29 15:50:14 +0900

    feat : UIDirector SetActive, SetDeactive 추가 , tag 못찾으면 return

--- TeamProject/Client/Private/UIDirector.cpp
+++ TeamProject/Client/Private/UIDirector.cpp
@@ -18,14 +18,30 @@ void CUIDirector::Register(CUI_Object* uiObj)
 	CGameInstance::GetInstance()->Get_UIMgr()->Add_UIObject(uiObj, m_levelKey);
 }
 
-void CUIDirector::SetVisible(const string& tag, bool visible)
+void CUIDirector::SetActive(const string& tag, void* arg)
 {
 	auto it = m_uiByTag.find(tag);
+	if (it == m_uiByTag.end()) return;
+	return it->second->UI_Active(arg);
+}
 
-	if (visible) 
-		it->second->Set_Alive(true);
-	else     
-		it->second->Set_Alive(false);
+void CUIDirector::SetActive(initializer_list<string> tags, void* arg)
+{
+	for (const auto& tag : tags)
+		SetActive(tag, arg);
+}
+
+void CUIDirector::SetDeactive(const string& tag, void* arg)
+{
+	auto it = m_uiByTag.find(tag);
+	if (it == m_uiByTag.end()) return;
+	return it->second->UI_DeActive();
+}
+
+void CUIDirector::SetDeactive(initializer_list<string> tags, void* arg)
+{
+	for (const auto& tag : tags)
+		SetDeactive(tag, arg);
 }
 
 void CUIDirector::Free()
--- TeamProject/Client/Public/UIDirector.h
+++ TeamProject/Client/Public/UIDirector.h
@@ -19,7 +19,12 @@ public:
 	void Initialize(const string& levelKey);
 
 	void Register(CUI_Object* uiObj);
-	void SetVisible(const string& tag, bool visible);
+	
+	void SetActive(const string& tag, void* arg = {});
+	void SetActive(initializer_list<string> tags, void* arg = {});
+	
+	void SetDeactive(const string& tag, void* arg = {});
+	void SetDeactive(initializer_list<string> tags, void* arg = {});
 
 private:
 	string                             m_levelKey;
