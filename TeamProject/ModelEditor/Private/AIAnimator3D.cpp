0d80dc86d5d876ef0018a05e7b6ddc1a1c74517e
boogiu <boogiww@gmail.com>
2026-01-02 17:54:13 +0900

    feat : Lod Proceed
    LOD 효율 점검, 효율적일지 의심됨

--- TeamProject/ModelEditor/Private/AIAnimator3D.cpp
+++ TeamProject/ModelEditor/Private/AIAnimator3D.cpp
@@ -51,6 +51,10 @@ HRESULT CAIAnimator3D::Initialize(const aiScene* pAIScene, CAIModelData* pAIMode
 	}
 
 	Resize_Layer(1);
+	m_AnimLayers[0].BaseLayer = true;
+	m_AnimLayers[0].eLayerType = ANIM_LAYER_STATE::BASE;
+	m_AnimLayers[0].fLayerWeight = 1.f;
+	m_AnimLayers[0].iRootBoneIndex = m_pData->Find_BoneIndexByName("Root");
 
 	return S_OK;
 }
