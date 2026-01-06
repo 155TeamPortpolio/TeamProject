54044772ad2c5271561054df20948861c4af8993
na4742 <126836684+na4742@users.noreply.github.com>
2026-01-06 14:05:53 +0900

    feat : 애니매이션 블랜드간 전환 수정 #541

--- TeamProject/Engine/Public/Animator3D.h
+++ TeamProject/Engine/Public/Animator3D.h
@@ -241,7 +241,7 @@ public: //»À °ü·Ã
 
     //TPose
     const vector<_float4x4>& Get_TPose() { return m_TPose; };
-    const _float4x4* Get_OwnerWorldMatrix() { return m_pOwner->Get_WorldMatrix(); }
+    const _float4x4* Get_OwnerWorldMatrix();
 
 public: /* DynamicBone */
     class CDynamicBone* Get_DynamicBone_Ptr() { 
