fcfc6ea0950ecea946fc76b7f6e490a96bea4ebd
yeji-gwon <ehclemf0526@gmail.com>
2026-01-07 21:27:54 +0900

    feat : battleHUD에 텍스트 추가해서 json 갱신하고, battleHUD에 핸들 저장 코드 추가 #529

--- TeamProject/Client/Private/UI_BattleHUD.cpp
+++ TeamProject/Client/Private/UI_BattleHUD.cpp
@@ -2,6 +2,7 @@
 #include "UI_BattleHUD.h"
 
 #include "GameInstance.h"
+#include "TextSlot.h"
 
 HRESULT CUI_BattleHUD::Initialize_Prototype()
 {
@@ -61,14 +62,14 @@ void CUI_BattleHUD::CacheHandle(CUI_Object* pRoot)
         m_hChildren[ULTIMATE_PREFABS[i]] = pRoot->Get_DescendantHandle("ultimate" + to_string(i + 1));
     }
 
-    //m_hChildren[PREFAB::CUR_HP_TEXT] = ;
-    //m_hChildren[PREFAB::MAX_HP_TEXT] = ;
+    m_hChildren[PREFAB::CUR_HP_TEXT] = pRoot->Get_DescendantHandle("curHpText");
+    m_hChildren[PREFAB::MAX_HP_TEXT] = pRoot->Get_DescendantHandle("maxHpText");
 
     m_hChildren[PREFAB::BOSS_ICON] = pRoot->Get_DescendantHandle("bossIcon");
     m_hChildren[PREFAB::BOSS_HP_BACK] = pRoot->Get_DescendantHandle("bossHpBack");
     m_hChildren[PREFAB::BOSS_HP_FRONT] = pRoot->Get_DescendantHandle("bossHpFront");
     m_hChildren[PREFAB::BOSS_GROGGY] = pRoot->Get_DescendantHandle("bossGroggy");
-    //m_hChildren[PREFAB::BOSS_GROGGY_TEXT] = pPrefab->Get_DescendantHandle("bossGroggyText");
+    m_hChildren[PREFAB::BOSS_GROGGY_TEXT] = pRoot->Get_DescendantHandle("bossGroggyText");
 
     //m_hChildren[PREFAB::BTN_NORMAL] = ;
     //m_hChildren[PREFAB::BTN_EVADE] = ;
