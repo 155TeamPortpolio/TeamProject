1c14dfd66e05f924fb8b2def8c41cbc8c05b01f4
yeji-gwon <ehclemf0526@gmail.com>
2026-01-09 21:16:23 +0900

    refactor : decibel의 kanji, digits 자식들 state, color 같은 변수 arg로 넘겨주게 수정 #529

--- TeamProject/Client/Private/UI_Decibel.cpp
+++ TeamProject/Client/Private/UI_Decibel.cpp
@@ -7,49 +7,6 @@
 #include "UI_DecibelKanji.h"
 #include "UI_DecibelDigits.h"
 
-void CUI_Decibel::Set_Decibel(_int iDecibel)
-{
-    if (iDecibel < 0 || iDecibel > 9999)
-        return;
-
-    m_iDecibel = iDecibel;
-
-    auto pKanji = dynamic_cast<CUI_DecibelKanji*>(Get_Slot(ChildSlot::KANJI));
-    auto pDigits = dynamic_cast<CUI_DecibelDigits*>(Get_Slot(ChildSlot::DIGITS));
-
-    if (!pKanji || !pDigits)
-        return;
-
-    // 간지 텍스쳐 셋 하고 (간지에서 분기하는게 나은가?)
-    CUI_DecibelKanji::State kanjiState = { CUI_DecibelKanji::State::NONE };
-
-    if (m_iDecibel >= 3000)
-    {
-        kanjiState = CUI_DecibelKanji::State::COMBAT_MAXIMUM;
-        m_vTargetColor = Helper::HexToColor("#FF9A22");
-    }
-    else if (m_iDecibel >= 2000)
-    {
-        kanjiState = CUI_DecibelKanji::State::COMBAT_BLASTING;
-        m_vTargetColor = Helper::HexToColor("#FFFF3E");
-    }
-    else if (m_iDecibel >= 2000)
-    {
-        kanjiState = CUI_DecibelKanji::State::COMBAT_UPROAR;
-        m_vTargetColor = Helper::HexToColor("#41FDFE");
-    }
-    else
-    {
-        kanjiState = CUI_DecibelKanji::State::COMBAT_MAXIMUM;   //None 처리해야
-        m_vTargetColor = Helper::HexToColor("#FFFFFF");
-    }
-    
-    pKanji->Set_Kanji(kanjiState);      // 데시벨 보내고 안에서 분길하는게 나은가
-    pDigits->Set_Digits(m_iDecibel);
-
-    pDigits->Set_AnchorOffset(_float2(Get_Slot(ChildSlot::KANJI)->Get_PxSize().x, 0.f));
-}
-
 HRESULT CUI_Decibel::Initialize_Prototype()
 {
     __super::Initialize_Prototype();
@@ -65,36 +22,81 @@ HRESULT CUI_Decibel::Initialize(INIT_DESC* pArg)
 
     Ready_PartObjects();
 
-    Set_Decibel(m_iDecibel);
-
 	return S_OK;
 }
 
 void CUI_Decibel::Update(_float dt)
 {
-    //Get_Component<CObjectContainer>()->UpdateChild(dt);
-    //
-    //if (CGameInstance::GetInstance()->Get_InputDev()->Key_Down('P'))
-    //    Set_Decibel(m_iDecibel + 12);
-    //
-    //if (CGameInstance::GetInstance()->Get_InputDev()->Key_Down('Q'))
-    //    Set_Decibel(m_iDecibel - 12);
+    Get_Component<CObjectContainer>()->UpdateChild(dt);
+
+    if (!m_initLayout)
+    {
+        Set_Decibel(0);
+        m_initLayout = true;
+    }
 
     _float fLerpAmount = min(1.f, dt * m_fColorLerpSpeed);
-    XMStoreFloat4(&m_vCurrentColor, XMVectorLerp(XMLoadFloat4(&m_vCurrentColor), XMLoadFloat4(&m_vTargetColor), fLerpAmount));
+    XMStoreFloat4(&m_vColor, XMVectorLerp(XMLoadFloat4(&m_vColor), XMLoadFloat4(&m_vTargetColor), fLerpAmount));
+
+    /*테스트 코드*/
+    if (CGameInstance::GetInstance()->Get_InputDev()->Key_Down('P'))
+        Set_Decibel(m_iDecibel + 12);
+
+    if (CGameInstance::GetInstance()->Get_InputDev()->Key_Down('Q'))
+        Set_Decibel(m_iDecibel - 12);
 }
 
 void CUI_Decibel::Ready_PartObjects()
 {
-    auto pContainer = Get_Component<CObjectContainer>();
+    auto pContainer = Get_Component<CObjectContainer>();;
 
+    CUI_DecibelKanji::KANJI_DESC* pKanji = new CUI_DecibelKanji::KANJI_DESC;
+    pKanji->pState = &m_iState;
+    pKanji->pColor = &m_vColor; 
     pContainer->Add_Child(
         Builder::Create_UIObject({ CGameInstance::GetInstance()->Get_LevelMgr()->Get_NowLevelKey(), "Proto_GameObject_DecibelKanji" })
+        .Add_UIDesc(pKanji)
         .Build("decibelKanji"));
 
+    CUI_DecibelDigits::DIGITS_DESC* pDigits = new CUI_DecibelDigits::DIGITS_DESC;
+    pDigits->pDecibel = &m_iDecibel;
+    pDigits->pColor = &m_vColor;
+    pDigits->pOffsetX = &m_fDigitsOffsetX;
     pContainer->Add_Child(
         Builder::Create_UIObject({ CGameInstance::GetInstance()->Get_LevelMgr()->Get_NowLevelKey(), "Proto_GameObject_DecibelDigits" })
+        .Add_UIDesc(pDigits)
         .Build("decibelDigits"));
+}
+
+void CUI_Decibel::Set_Decibel(_int iDecibel)
+{
+    if (iDecibel < 0 || iDecibel > 9999)
+        return;
+
+    m_iDecibel = iDecibel;
+
+    if (m_iDecibel >= 3000)
+    {
+        m_iState = State::COMBAT_MAXIMUM;
+        m_vTargetColor = Helper::HexToColor("#FF9A22");
+    }
+    else if (m_iDecibel >= 2000)
+    {
+        m_iState = State::COMBAT_BLASTING;
+        m_vTargetColor = Helper::HexToColor("#FFFF3E");
+    }
+    else if (m_iDecibel >= 1000)
+    {
+        m_iState = State::COMBAT_UPROAR;
+        m_vTargetColor = Helper::HexToColor("#41FDFE");
+    }
+    else
+    {
+        m_iState = State::NONE;
+        m_vTargetColor = Helper::HexToColor("#FFFFFF");
+    }
+
+    m_fDigitsOffsetX = (Get_Slot(ChildSlot::KANJI)) ? Get_Slot(ChildSlot::KANJI)->Get_PxSize().x * 0.9f : 100.f;
 }
 
 CUI_Object* CUI_Decibel::Get_Slot(ChildSlot slot)
--- TeamProject/Client/Public/UI_DecibelKanji.h
+++ TeamProject/Client/Public/UI_DecibelKanji.h
@@ -1,15 +1,19 @@
 #pragma once
 #include "UI_Object.h"
+#include "UI_Decibel.h"
 
 NS_BEGIN(Client)
 
 class CUI_DecibelKanji final : public CUI_Object
 {
 public:
-	enum class State { NONE, COMBAT_UPROAR, COMBAT_BLASTING, COMBAT_MAXIMUM, END };
+	typedef struct tagKanjiDesc : public UI_DESC {
+		const _uint*	pState = { nullptr };
+		const _float4*	pColor = { nullptr };
+	}KANJI_DESC;
 
 private:
-	static const string KANJI_TEXTURES[ENUM(State::END)];
+	static const string KANJI_TEXTURES[ENUM(CUI_Decibel::State::END)];
 
 	enum class ChildSlot { BG, KANJI, END };
 
@@ -17,9 +21,6 @@ private:
 	CUI_DecibelKanji() {}
 	CUI_DecibelKanji(const CUI_DecibelKanji& rhs) : CUI_Object(rhs) {}
 	virtual ~CUI_DecibelKanji() DEFAULT;
-
-public:
-	void Set_Kanji(State texture);
 
 public:
 	virtual HRESULT Initialize_Prototype()           override;
@@ -33,7 +34,12 @@ private:
 	const _float	m_fHeight = 50.f;
 	const _vector2	m_vPadding = { 10.f, 10.f };
 
+	const _uint*	m_pState = { nullptr };
+	const _float4*	m_pColor = { nullptr };
+
 private:
+	void Ready_PartObjects();
+	void Set_Kanji(CUI_Decibel::State texture);
 	void Set_KanjiTexture(CUI_Object* pKanji, string textureKey);
 	void Set_Layout(CUI_Object* pKanji, CUI_Object* pBg);
 
