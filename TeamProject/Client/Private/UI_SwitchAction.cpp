f792b0408e161bb4d4e3c5247d2f9f766230000d
yeji-gwon <ehclemf0526@gmail.com>
2026-01-13 16:18:59 +0900

    chore : UI Action 테스트 코드 주석 #529

--- TeamProject/Client/Private/UI_SwitchAction.cpp
+++ TeamProject/Client/Private/UI_SwitchAction.cpp
@@ -55,38 +55,38 @@ HRESULT CUI_SwitchAction::Initialize(INIT_DESC* pArg)
 void CUI_SwitchAction::Update(_float dt)
 {
 	// 이벤트 테스트 코드
-	if (CGameInstance::GetInstance()->Get_InputDev()->Key_Down('M'))
-	{
-	    UI_ACTION_DESC desc = {};
-	    desc.eType = UI_ACTION_TYPE::SWITCH;
-	    desc.eState = UI_ACTION_STATE::DISABLE;
-	    EventSystem()->Broadcast<UI_ACTION_DESC>({ desc });
-	}
-	
-	if (CGameInstance::GetInstance()->Get_InputDev()->Key_Down('N'))
-	{
-	    UI_ACTION_DESC desc = {};
-	    desc.eType = UI_ACTION_TYPE::SWITCH;
-	    desc.eState = UI_ACTION_STATE::ENABLE;
-	    EventSystem()->Broadcast<UI_ACTION_DESC>({ desc });
-	}
-	
-	if (CGameInstance::GetInstance()->Get_InputDev()->Key_Down('B'))
-	{
-	    UI_ACTION_DESC desc = {};
-	    desc.eType = UI_ACTION_TYPE::SWITCH;
-	    desc.eState = UI_ACTION_STATE::AVAILABLE;
-	    EventSystem()->Broadcast<UI_ACTION_DESC>({ desc });
-	}
-	 
-	if (CGameInstance::GetInstance()->Get_InputDev()->Key_Down('V'))
-	{
-		UI_ACTION_DESC desc = {};
-		desc.eType = UI_ACTION_TYPE::SWITCH;
-		desc.eState = UI_ACTION_STATE::EXECUTING;
-		desc.fFillAmount = 0.75f;
-		EventSystem()->Broadcast<UI_ACTION_DESC>({ desc });
-	}
+	//if (CGameInstance::GetInstance()->Get_InputDev()->Key_Down('M'))
+	//{
+	//    UI_ACTION_DESC desc = {};
+	//    desc.eType = UI_ACTION_TYPE::SWITCH;
+	//    desc.eState = UI_ACTION_STATE::DISABLE;
+	//    EventSystem()->Broadcast<UI_ACTION_DESC>({ desc });
+	//}
+	//
+	//if (CGameInstance::GetInstance()->Get_InputDev()->Key_Down('N'))
+	//{
+	//    UI_ACTION_DESC desc = {};
+	//    desc.eType = UI_ACTION_TYPE::SWITCH;
+	//    desc.eState = UI_ACTION_STATE::ENABLE;
+	//    EventSystem()->Broadcast<UI_ACTION_DESC>({ desc });
+	//}
+	//
+	//if (CGameInstance::GetInstance()->Get_InputDev()->Key_Down('B'))
+	//{
+	//    UI_ACTION_DESC desc = {};
+	//    desc.eType = UI_ACTION_TYPE::SWITCH;
+	//    desc.eState = UI_ACTION_STATE::AVAILABLE;
+	//    EventSystem()->Broadcast<UI_ACTION_DESC>({ desc });
+	//}
+	// 
+	//if (CGameInstance::GetInstance()->Get_InputDev()->Key_Down('V'))
+	//{
+	//	UI_ACTION_DESC desc = {};
+	//	desc.eType = UI_ACTION_TYPE::SWITCH;
+	//	desc.eState = UI_ACTION_STATE::EXECUTING;
+	//	desc.fFillAmount = 0.75f;
+	//	EventSystem()->Broadcast<UI_ACTION_DESC>({ desc });
+	//}
 
 	Get_Component<CObjectContainer>()->UpdateChild(dt);
 }
