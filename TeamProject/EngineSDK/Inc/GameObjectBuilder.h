a2dcd0da03667c5c32dc22e601b049f5aafddbed
boogiu <86179065+boogiu@users.noreply.github.com>
2026-01-15 11:40:05 +0900

    feat : 기능 추가 완료 테스트 필요 #726

--- TeamProject/EngineSDK/Inc/GameObjectBuilder.h
+++ TeamProject/EngineSDK/Inc/GameObjectBuilder.h
@@ -6,6 +6,7 @@ NS_BEGIN(Engine)
 class ENGINE_DLL CGameObjectBuilder
 {
 public:
+public:
     CGameObjectBuilder(const CLONE_DESC& cloneDesc);
     ~CGameObjectBuilder();
 public:
@@ -25,7 +26,8 @@ public:
    //CGameObjectBuilder& Sphere_Collider(const SPHERE_COLLIDER_DESC& desc);
 
     CGameObjectBuilder& Set_Parent(const PARENT_DESC& parent);
-
+    CGameObjectBuilder& Model_Link(const MODEL_INIT_DESC& pArg, MESH_TYPE eType);
+    CGameObjectBuilder& Material_Link(const MATERIAL_INIT_DESC& pArg);
 
     CGameObjectBuilder& Add_ObjDesc(GAMEOBJECT_DESC* pArg);
     /*동적 생성 아님*/
