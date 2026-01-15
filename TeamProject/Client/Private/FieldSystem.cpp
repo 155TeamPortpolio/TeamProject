80651a1eeed7bcc52f70ae7c2197d1fb64dee7e4
boogiu <86179065+boogiu@users.noreply.github.com>
2026-01-15 19:33:32 +0900

    feat : Add Field System #746

---
+++ TeamProject/Client/Private/FieldSystem.cpp
@@ -0,0 +1,45 @@
+#include "pch.h"
+#include "FieldSystem.h"
+#include "FieldPlayer.h"
+
+IMPLEMENT_SINGLETON(CFieldSystem)
+
+CFieldSystem::CFieldSystem()
+{
+}
+
+void CFieldSystem::Update()
+{
+	if (false == m_isActive)
+		return;
+}
+
+void CFieldSystem::SetFieldPlayer(CFieldPlayer* pFieldPlayer)
+{
+	if (pFieldPlayer)
+	{
+		if (m_pFieldPlayer)
+		{
+			Safe_Release(m_pFieldPlayer);
+			m_pFieldPlayer = nullptr;
+		}
+
+		m_pFieldPlayer = pFieldPlayer;
+		Safe_AddRef(m_pFieldPlayer);
+	}
+}
+
+/*isValid 체크 필요!*/
+OBJECT_HANDLE CFieldSystem::GetCurCharacterHandle() const
+{
+	if (m_pFieldPlayer)
+		return m_pFieldPlayer->Get_CurCharacterHandle();
+	else
+		return OBJECT_HANDLE();
+}
+
+void CFieldSystem::Free()
+{
+	__super::Free();
+	Safe_Release(m_pFieldPlayer);
+}
