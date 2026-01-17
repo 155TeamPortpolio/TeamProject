ecfdf01080c52faf716e864affd25c85934b2f27
yugi0714 <45875223+yugi0714@users.noreply.github.com>
2026-01-17 17:59:44 +0900

    Feat:회피 및 셰이딩 처리(Evade) #766

--- TeamProject/Client/Public/BattleSystem.h
+++ TeamProject/Client/Public/BattleSystem.h
@@ -8,38 +8,69 @@ NS_END
 NS_BEGIN(Client)
 class CBattlePlayer;
 
+enum class BATTLE_VFX_TYPE { EVADE, END };
+
+#pragma region Struct
 // TimeScale 실행 시 시간관리용(저장용X)
 typedef struct tagTimeScale 
 {
-	_bool	isScaled = { false };
+	_bool	isRunning = { false };
 	_float	fDuration = {};
 	_float	fCurPos = {};
-	_float	fLerpTimeRatio = {};
 	_float	fScaleValue = { 1.f };
-}TIME_SCALE;
+	
+	_float2	vStartLerpTime = {};	/* x : duration, y : curpos */
+	_float2	vEndLerpTime = {};		/* x : duration, y : curpos */
+	//EaseType eEaseType = EaseType::None;
 
-typedef struct tagBattleVFX 
+	_float	GetTimeRatio() 
+	{ 
+		if (0 > fCurPos)	
+			return 0.f;		// 0 나누기 방지
+		return clamp(fCurPos / fDuration, 0.f, 1.f); 
+	}              
+}TIME_SCALING;
+
+typedef struct tagTimeScaleData
 {
-	_bool	isRunning = { false };
-	_float	fVFXDuration = {};
-	_float	fVFXCurrentPos = {};
+	_float	fDuration = {};
+	_float	fValue = { 1.f };			// dt에 스케일 할 값 (dt * fValue)
+	_float	fStartLerpTime = { 0.f };	// 스케일 시작 보간 비율 (0~1)
+	_float	fEndLerpTime = { 0.2f };	// 스케일 종료 보간 비율 (0~1)
+	//EaseType eEaseType = EaseType::None;
+}TIME_SCALE_DATA;
 
-	_float	fPlayerTimeScaleDuration = {};
-	_float	fPlayerTimeScaleValue = {};
-	_float	fMonsterTimeScaleDuration = {};
-	_float	fMonsterTimeScaleValue = {};
-	_float	fEffectTimeScaleDuration = {};
-	_float	fEffectTimeScaleValue = {};
-
-	_float	GetVFXTimeRatio() { return fVFXCurrentPos / fVFXDuration; }
+/* fDuration, fValue, fStartLerpTime */  
+typedef struct tagBattleVFX
+{
+	BATTLE_VFX_TYPE eVFXType = BATTLE_VFX_TYPE::END;
+	_bool	isRunning = {};
+	_float	fDuration = {};
+	_float	fCurPos = {};
+	_float3 vLerpColor = {};
+	_float	GetTimeRatio()				
+	{
+		if (0 > fCurPos)
+			return 0.f;		// 0 나누기 방지
+		return clamp(fCurPos / fDuration, 0.f, 1.f);
+	}
 }BATTLE_VFX;
+
+typedef struct tagBattleVFXData
+{
+	_float			fVFXDuration = {};			/* 화면 색 먹이는 시간 */
+	_float			fBlurDuration = {};			/* 블러 먹이는 시간 */
+	TIME_SCALE_DATA tPlayerTimeScale = {};		/* Duration, ScaleValue(0 < value < 1), StartLerpTime, EndLerpTime */
+	TIME_SCALE_DATA tMonsterTimeScale = {};		/* Duration, ScaleValue(0 < value < 1), StartLerpTime, EndLerpTime */
+	TIME_SCALE_DATA tEffectTimeScale = {};		/* Duration, ScaleValue(0 < value < 1), StartLerpTime, EndLerpTime */
+}BATTLE_VFX_DATA;
+#pragma endregion
 
 class CBattleSystem final : public CBase
 {
 	DECLARE_SINGLETON(CBattleSystem)
 public:
 	enum class BATTLE_OBJ_TYPE { PLAYER, MONSTER, ENVOBJECT, END };
-	enum class BATTLE_VFX_TYPE { EVADE , END};
 private:
 	CBattleSystem();
 	virtual ~CBattleSystem() = default;
@@ -61,7 +92,7 @@ public: //getter
 
 
 	/* 테스트용! 금방 지울예정 - 경인 */
-	unordered_map<BATTLE_OBJ_TYPE, TIME_SCALE>* GetTimeScales() { return &m_TimeScales; }
+	vector<TIME_SCALING>* GetTimeScales() { return &m_TimeScaling; }
 
 
 public: //setter
@@ -72,15 +103,18 @@ public: //setter
 	// UI에서 캐릭터 선택시 부를 함수
 	void	SetBattleCharacters(vector<CHARACTER> battleCharacters);
 	void	SetBattlePlayer(class CBattlePlayer* pBattlePlayer) { m_pBattlePlayer = pBattlePlayer; }
-	void	StartTimeScale(BATTLE_OBJ_TYPE eObjType, _float fDuration, _float fScale);
-	void	StartShaderVFX(BATTLE_VFX_TYPE eVFXType, _float fDuration);
+	// 기믹 성공 시 호출 함수(Evade, 지원공격 등)
+	void	StartGimmick(BATTLE_VFX_TYPE eVFXType);
+
+	// 아래 함수들은 추후에 private로 넘길 예정
+	void	StartTimeScale(BATTLE_OBJ_TYPE eObjType, _float fDuration, _float fScale, _float fStartLerpTime = 0.f, _float fEndLerpTime = 0.f);
+	void	StartShaderVFX(BATTLE_VFX_TYPE eVFXType);
 
 private:
 	void	Update_BattleInfo();
 	void	ClearBattleStage();
 	void	CheckTimeScale(const _float dt);
 	void	CheckVFX(const _float dt);
-	void	ComputeVFXValue(const _float dt, _uint iVFXIndex);
 
 private:
 	_bool	m_isActive = { false };
@@ -91,10 +125,13 @@ private:
 	unordered_map<BATTLE_OBJ_TYPE, vector<OBJECT_HANDLE>>	m_Handles;
 	// 매 업데이트때 1번씩 생성된 Battle 오브젝트의 정보를 담아둠
 	unordered_map<BATTLE_OBJ_TYPE, vector<BATTLEOBJ_INFO>>	m_BattleObjInfos;
-	// BATTLE_OBJ_TYPE 별로 타임 스케일
-	unordered_map<BATTLE_OBJ_TYPE, TIME_SCALE>				m_TimeScales;
-	// 쉐이더 효과
-	vector<BATTLE_VFX>										m_BattleVFX;
+	// BATTLE_VFX_TYPE 별로 쉐이더 효과 시작 시 필요한 데이터
+	vector<BATTLE_VFX_DATA>		m_BattleVFXData;
+	// BATTLE_OBJ_TYPE 별로 타임 스케일 관리용
+	vector<TIME_SCALING>		m_TimeScaling;
+	//쉐이더 효과 시간 관리용
+	BATTLE_VFX					m_BattleVFX;
+	//vector<BATTLE_VFX>			m_BattleVFX;
 
 	const _char* m_LayerTag[2] = { "Model_Layer", "Enemy_Layer" };
 
