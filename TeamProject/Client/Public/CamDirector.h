e80a747bc967196862384abef95a920f1376c668
jehyun1108 <andixuan_1108@naver.com>
2026-01-19 01:58:26 +0900

    Merge

--- TeamProject/Client/Public/CamDirector.h
+++ TeamProject/Client/Public/CamDirector.h
@@ -72,6 +72,6 @@ private:
     _int                    m_focusType = -1;
 };
 
-inline auto& CamDirector() { return *CCamDirector::GetInstance(); }
+inline auto* CamDirector() { return CCamDirector::GetInstance(); }
 
 NS_END
