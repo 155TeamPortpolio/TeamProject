2a17685016776470efbaaef160be934110349083
yeji-gwon <ehclemf0526@gmail.com>
2026-01-21 21:17:58 +0900

    feat : interactable 메시지 받으면 이름표 화살표 보이게, 안 보이게 처리 #905

--- TeamProject/Client/Private/UI_NameIndicator.cpp
+++ TeamProject/Client/Private/UI_NameIndicator.cpp
@@ -3,6 +3,7 @@
 
 #include "GameInstance.h"
 #include "ObjectContainer.h"
+#include "EventListener.h"
 #include "CharacterController.h"
 
 #include "TextSlot.h"
@@ -13,6 +14,7 @@ HRESULT CUI_NameIndicator::Initialize_Prototype()
     __super::Initialize_Prototype();
 
     Add_Component<CObjectContainer>();
+    Add_Component<CEventListener>();
 
     return S_OK;
 }
@@ -28,6 +30,17 @@ HRESULT CUI_NameIndicator::Initialize(INIT_DESC* pArg)
     Load(Helper::LoadJson<nlohmann::ordered_json>(ResourceManager()->Get_ResourcePath("name_indicator.json")));
     Cache_Children();
 
+    Get_Component<CEventListener>()->Add_Listener<UI_INTERACTABLE_DESC>([&](const UI_INTERACTABLE_DESC& desc)
+        {
+            if (desc.strName != m_strName)
+                return;
+
+            if(desc.isInteractable)
+                Update_State(STATE::INTERACTABLE);
+            else
+                Update_State(STATE::NOTINTERACTABLE);
+        });
+
     return S_OK;
 }
 
@@ -36,10 +49,28 @@ void CUI_NameIndicator::Awake()
     Set_Name(m_strName);
 
     Set_Alpha(0.f);
+
+    Set_ChildAlive(CHILD::ARROWL, false);
+    Set_ChildAlive(CHILD::ARROWR, false);
 }
 
 void CUI_NameIndicator::Update(_float dt)
 {
+    //if (InputDevice()->Key_Down('P'))
+    //{
+    //    UI_INTERACTABLE_DESC desc = {};
+    //    desc.isInteractable = true;
+    //    desc.strName = m_strName;
+    //    EventSystem()->Broadcast<UI_INTERACTABLE_DESC>({ desc });
+    //}
+    //if (InputDevice()->Key_Down('O'))
+    //{
+    //    UI_INTERACTABLE_DESC desc = {};
+    //    desc.isInteractable = false;
+    //    desc.strName = m_strName;
+    //    EventSystem()->Broadcast<UI_INTERACTABLE_DESC>({ desc });
+    //}
+
     if(m_pCCT)
         XMStoreFloat3(&m_vPosition, m_pCCT->Get_FootPosition() + XMVectorSet(0.f, m_pCCT->Get_HalfSize() * 2.f, 0.f, 0.f));
 
@@ -92,6 +123,20 @@ void CUI_NameIndicator::Update_State(STATE eNewState)
         Set_Animation(0);
     else if (m_eState == STATE::HIDDEN)
         Set_Animation(1);
+    else if (m_eState == STATE::INTERACTABLE)
+    {
+        Set_ChildAlive(CHILD::ARROWL, true);
+        Set_ChildAlive(CHILD::ARROWR, true);
+        //Set_ChildAnimation(CHILD::ARROWL, 0);
+        //Set_ChildAnimation(CHILD::ARROWR, 0);
+    }
+    else if (m_eState == STATE::NOTINTERACTABLE)
+    {
+        Set_ChildAlive(CHILD::ARROWL, false);
+        Set_ChildAlive(CHILD::ARROWR, false);
+        //Set_ChildAnimation(CHILD::ARROWL, 0);
+        //Set_ChildAnimation(CHILD::ARROWR, 0);
+    }
 }
 
 CUI_NameIndicator::STATE CUI_NameIndicator::CalcState_ByDistance()
@@ -102,6 +147,24 @@ CUI_NameIndicator::STATE CUI_NameIndicator::CalcState_ByDistance()
     Vector3 vDiff = (Vector3(m_vPosition) - Vector3(vPlayerPos));
 
     return (max(m_fRadius, 1.f) >= vDiff.Length()) ? STATE::VISIBLE : STATE::HIDDEN;
+}
+
+void CUI_NameIndicator::Set_ChildAlive(CHILD child, _bool isAlive)
+{
+    auto pChild = m_pChildren[child];
+    if (!pChild)
+        return;
+
+    pChild->Set_Alive(isAlive);
+}
+
+void CUI_NameIndicator::Set_ChildAnimation(CHILD child, _int iIndex)
+{
+    auto pChild = m_pChildren[child];
+    if (!pChild)
+        return;
+
+    pChild->Set_Animation(iIndex);
 }
 
 _float2 CUI_NameIndicator::Get_ChildSize(CHILD child)
