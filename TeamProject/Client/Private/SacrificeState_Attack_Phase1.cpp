cc103180f2a8f690bfb04502ec014b000b6fdfc0
woo <leewooseok1114@gmail.com>
2026-01-21 14:23:47 +0900

    feat : 새크리파이스 이펙트 수정 #930

--- TeamProject/Client/Private/SacrificeState_Attack_Phase1.cpp
+++ TeamProject/Client/Private/SacrificeState_Attack_Phase1.cpp
@@ -172,11 +172,11 @@ void CSacrificeState_Attack_Phase1::BuildPattern(CSacrifice* pOwner)
 		}
 	}
 	blackBoard.stateQueue.clear();
-	blackBoard.stateQueue.push_back("Attack01_Phase1");
-	blackBoard.stateQueue.push_back("Attack02_Phase1");
-	blackBoard.stateQueue.push_back("Attack03_Phase1");
-	blackBoard.stateQueue.push_back("Attack08_Phase1");
-	blackBoard.stateQueue.push_back("Attack08_Phase1");
+	blackBoard.stateQueue.push_back("Attack05_Phase1");
+	blackBoard.stateQueue.push_back("Attack05_Phase1");
+	blackBoard.stateQueue.push_back("Attack05_Phase1");
+	blackBoard.stateQueue.push_back("Attack05_Phase1");
+	blackBoard.stateQueue.push_back("Attack05_Phase1");
 	blackBoard.stateQueue.push_back("Attack_Turn_Phase1");
 	
 	blackBoard.isRequestNext = true;
--- TeamProject/Client/Private/Zero_Level.cpp
+++ TeamProject/Client/Private/Zero_Level.cpp
@@ -58,12 +58,12 @@ HRESULT CZero_Level::Initialize()
 	m_Context.hPlayer = castedPlayer->Get_CurCharacterHandle();
 
 	/* Cloud */
-	PrototypeManager()->Add_ProtoType("Zero_Level", "Proto_GameObject_TestCloud", CTestCloud::Create());
-	auto testCloud = Builder::Create_Object({ "Zero_Level", "Proto_GameObject_TestCloud" })
-		.Scale(_float3(2.f, 2.f, 2.f))
-		.Build("Test_Cloud");
-	
-	ObjectManager()->Add_Object(testCloud, {"Zero_Level", "Etc_Layer"});
+	//PrototypeManager()->Add_ProtoType("Zero_Level", "Proto_GameObject_TestCloud", CTestCloud::Create());
+	//auto testCloud = Builder::Create_Object({ "Zero_Level", "Proto_GameObject_TestCloud" })
+	//	.Scale(_float3(2.f, 2.f, 2.f))
+	//	.Build("Test_Cloud");
+	//
+	//ObjectManager()->Add_Object(testCloud, {"Zero_Level", "Etc_Layer"});
 
 	PrototypeManager()->Add_ProtoType("Zero_Level", "Proto_GameObject_Sacrifice", CSacrifice::Create());
 	PrototypeManager()->Add_ProtoType("Zero_Level", "Proto_GameObject_SacrificeHand", CSacrificeHand::Create());
@@ -131,6 +131,7 @@ HRESULT CZero_Level::Initialize()
 		pResource->Add_ResourcePath("Eff_Particle_009.png", "../Bin/Resources/Effect/Texture/Eff_Particle_009.png");
 		pResource->Add_ResourcePath("Eff_Flare_060.png", "../Bin/Resources/Effect/Texture/Eff_Flare_060.png");
 		pResource->Add_ResourcePath("Eff_Trail_140_LYF_01.png", "../Bin/Resources/Effect/Texture/Eff_Trail_140_LYF_01.png");
+		pResource->Add_ResourcePath("VX_Noise_UU_16.png", "../Bin/Resources/Effect/Texture/VX_Noise_UU_16.png");
 
 		/* Models */
 		pResource->Add_ResourcePath("Smoke_Cone2.model", "../Bin/Resources/Effect/Model/Sacrifice_Smoke_Trail/Smoke_Cone2.model");
