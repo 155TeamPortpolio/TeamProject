42cfbce404553d77d2c39010673e94966edd2b13
tballoon23 <asbssu18@gmail.com>
2026-01-25 14:51:46 +0900

    fix : Collision 버그 수정

--- TeamProject/Engine/Private/RigidBody.cpp
+++ TeamProject/Engine/Private/RigidBody.cpp
@@ -52,39 +52,38 @@ HRESULT CRigidBody::Initialize(COMPONENT_DESC* pArg)
 	PxScene* pScene = m_pPhysicsSystem->Get_Scene();
 	if (!pPhysics || !pScene) return E_FAIL;
 
+	RIGIDBODY_DESC desc;
 	if (pArg)
-	{
-		RIGIDBODY_DESC* pDesc = static_cast<RIGIDBODY_DESC*>(pArg);
-		m_bKinematic = pDesc->isKinematic;
-		m_bGravity = pDesc->bEnableGravity;
-		m_fMass = pDesc->fMass;
-		m_bLockX = pDesc->bLockX;
-		m_bLockY = pDesc->bLockY;
-		m_bLockZ = pDesc->bLockZ;
-		m_fLinearDamping = pDesc->fLinearDamping;
-		m_fAngularDamping = pDesc->fAngularDamping;
-	}
+		desc = *static_cast<RIGIDBODY_DESC*>(pArg);
 
-	// 초기 위치
+	m_bKinematic = desc.isKinematic;
+	m_bGravity = desc.bEnableGravity;
+	m_fMass = desc.fMass;
+	m_bLockX = desc.bLockX;
+	m_bLockY = desc.bLockY;
+	m_bLockZ = desc.bLockZ;
+	m_fLinearDamping = desc.fLinearDamping;
+	m_fAngularDamping = desc.fAngularDamping;
+
 	_vector vPos = m_pOwnerTransform->Get_WorldPos();
 	_smatrix mWorldMat = m_pOwnerTransform->Get_WorldMatrix();
 	_vector vScale, vRot, vTrans;
 	XMMatrixDecompose(&vScale, &vRot, &vTrans, mWorldMat);
-	PxTransform initialPose(ToPxVec3(vPos), ToPxQuat(vRot));
 
+	PxTransform initialPose(ToPxVec3(vPos), ToPxQuat(vRot));
 	m_pActor = pPhysics->createRigidDynamic(initialPose);
 	if (!m_pActor) return E_FAIL;
-	
+
 	m_pActor->setRigidBodyFlag(PxRigidBodyFlag::eKINEMATIC, m_bKinematic);
 	m_pActor->setActorFlag(PxActorFlag::eDISABLE_GRAVITY, !m_bGravity);
 	m_pActor->setMass(m_fMass);
 	m_pActor->setLinearDamping(m_fLinearDamping);
 	m_pActor->setAngularDamping(m_fAngularDamping);
-
 	m_pActor->setRigidDynamicLockFlag(PxRigidDynamicLockFlag::eLOCK_ANGULAR_X, m_bLockX);
 	m_pActor->setRigidDynamicLockFlag(PxRigidDynamicLockFlag::eLOCK_ANGULAR_Y, m_bLockY);
 	m_pActor->setRigidDynamicLockFlag(PxRigidDynamicLockFlag::eLOCK_ANGULAR_Z, m_bLockZ);
 	m_pActor->setSolverIterationCounts(8, 4);
+
 	if (!m_bKinematic)
 	{
 		m_pActor->setRigidBodyFlag(PxRigidBodyFlag::eENABLE_CCD, true);
@@ -92,9 +91,9 @@ HRESULT CRigidBody::Initialize(COMPONENT_DESC* pArg)
 	}
 
 	m_pActor->setMaxDepenetrationVelocity(1.0f);
-
 	m_pActor->setActorFlag(PxActorFlag::eSEND_SLEEP_NOTIFIES, true);
 	m_pActor->userData = m_pOwner;
+
 	pScene->addActor(*m_pActor);
 
 	return S_OK;
