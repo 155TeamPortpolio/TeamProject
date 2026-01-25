2774c623955f76a71700ef2931560f6ceda30e31
jehyun1108 <andixuan_1108@naver.com>
2026-01-25 22:36:40 +0900

    feat : UI DamageText #1019

--- TeamProject/Client/Private/MainApp.cpp
+++ TeamProject/Client/Private/MainApp.cpp
@@ -132,7 +132,7 @@ void CMainApp::Set_Levels()
 
 	LevelManager()->Set_LoadingLevel("Loading_Level");
 	m_pGameInstance->Notify_LevelSet(); 
-	m_pGameInstance->Get_LevelMgr()->Request_ChangeLevel("Test_Level",false); 
+	m_pGameInstance->Get_LevelMgr()->Request_ChangeLevel("Scott_Level",false); 
 } 
 
 CMainApp* CMainApp::Create()
