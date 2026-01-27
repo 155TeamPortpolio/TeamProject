1d7211a9446ac070b5a4b918e7b45702ff0b9700
tballoon23 <asbssu18@gmail.com>
2026-01-27 19:59:07 +0900

    chore : CounterAttack 수정

--- TeamProject/Client/Private/JaneDoeState_CounterAttack.cpp
+++ TeamProject/Client/Private/JaneDoeState_CounterAttack.cpp
@@ -30,20 +30,21 @@ CJaneDoeState_CounterAttack* CJaneDoeState_CounterAttack::Create()
 void CJaneDoeState_CounterAttack::Enter(CJaneDoe* pOwner)
 {
 	pOwner->Lock_Move();
+	pOwner->Push_Invincible();
 	if (pOwner->Is_Passion())
 	{
 		m_pSubStateMachine->Set_DefaultState("Counter_03");
 		m_pSubStateMachine->Set_Int("CounterMode", 0);
 	}
-	else if (pOwner->Get_EvadeCount() % 2 == 0)
-	{
-		m_pSubStateMachine->Set_DefaultState("Counter_02");
-		m_pSubStateMachine->Set_Int("CounterMode", 1);
-	}
-	else
+	else if (pOwner->Get_EvadeCount() <= 1)
 	{
 		m_pSubStateMachine->Set_DefaultState("Counter_01");
 		m_pSubStateMachine->Set_Int("CounterMode", 2);
+	}
+	else
+	{
+		m_pSubStateMachine->Set_DefaultState("Counter_02");
+		m_pSubStateMachine->Set_Int("CounterMode", 1);
 	}
 
 	__super::Enter(pOwner);
@@ -105,6 +106,7 @@ void CJaneDoeState_CounterAttack::Update(CJaneDoe* pOwner, _float dt)
 
 void CJaneDoeState_CounterAttack::Exit(CJaneDoe* pOwner)
 {
+	pOwner->Pop_Invincible();
 	__super::Exit(pOwner);
 }
 
