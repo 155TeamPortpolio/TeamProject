a0495cd9d3a03301448438740d8a17a5bd602f0e
yeji-gwon <ehclemf0526@gmail.com>
2026-01-27 19:30:40 +0900

    feat : 복권 스크래치된 정도가 일정 값을 넘으면 결과 보이게 #1034

--- TeamProject/Client/Public/UI_ScratchCard.h
+++ TeamProject/Client/Public/UI_ScratchCard.h
@@ -13,6 +13,9 @@ private:
 	enum class STATE { INVISIBLE, VISIBLE, END };
 	enum class ANIMATION { APPEAR, DISAPPEAR, IDLE, END };
 
+	enum class CHILD { BRUSH, SCRATCH, REWARD, END };
+	inline static const string INSTANCENMAES[ENUM(CHILD::END)] = { "brush", "scratch", "reward" };
+
 	enum class REWARD { REWARD1, REWARD2, REWARD3, REWARD4, REWARD5, END };
 	inline static const string REWARD_TEXTURES[ENUM(REWARD::END)] = { "ScratchCardRewardIcon01.png", "ScratchCardRewardIcon02.png",
 	"ScratchCardRewardIcon03.png", "ScratchCardRewardIcon04.png", "ScratchCardRewardIcon05.png" };
@@ -36,9 +39,8 @@ public:
 private:
 	STATE				m_eState = { STATE::END };
 
-	CUI_Object*			m_pBrush = {};
-	class CSprite2D*	m_pBrushSprite = {};
-	class CSprite2D*	m_pRewardSprite = {};
+	CUI_Object*			m_pChildren[ENUM(CHILD::END)] = {};
+	class CSprite2D*	m_pSprites[ENUM(CHILD::END)] = {};
 
 	/* 커스텀 렌더타겟 관련 변수들 */
 	_float4x4			m_ViewMatrix = {};			// 브러쉬 뷰
@@ -46,19 +48,35 @@ private:
 	_float				m_fThreshold = { -0.1f };	// 브러쉬로 그린 렌더타겟을 마스크로 쓸 때 흑백 반전시키려고
 	_bool				m_isClear = {};				// 렌더타겟 클리어
 
+	/* 스크래치 진행 정도 확인을 위한 변수들 */
+	_bool				m_isScratchComplete = {};	// 스크래치 완료
+	_float				m_fScratchTimer = {};		// 스크래치 확인 타이머
+	const _float	    m_fScratchDuration = { 1.f }; // 스크래치 확인 시간 간격
+	const _float		m_fScratchRatio = { 0.3f };	// 스크래치 정도
+
+	/* 결과배너 관련 변수들 */
+	_bool				m_hasShownResult = {};		// 결과 배너를 보여줌
+	_float				m_fResultWaitTime = {};		// 결과 배너 타이머
+	const _float		m_fResultWaitDuration = { 1.f };	// 결과 배너 몇 초 후에
+
 private:
-	void Cache_Brush();
-	void Cache_Reward();
+	void Cache();
+
+	_bool Check_Scratch(_float dt);
+	_float Calculate_ScratchRatio();
 
 	void Change_State(STATE eState);
 	void Change_RewardTexture(const string& strTextureKey);
+	 
+	void Reset();
 
 	void Render_RTBrush(ID3D11DeviceContext* pContext);
+	void Render_RT(CHILD child, ID3D11DeviceContext* pContext);
 
 public:
 	static  CGameObject* Create();
 	virtual CGameObject* Clone(INIT_DESC* pArg = {}) override;
-	virtual void Free() { __super::Free(); }
+	virtual void Free() override;
 };
 
 NS_END
