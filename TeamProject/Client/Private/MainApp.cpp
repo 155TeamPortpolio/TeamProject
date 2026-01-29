30424352ca036a89f2741970e3ab5d521125dab8
boogiu <86179065+boogiu@users.noreply.github.com>
2026-01-29 21:02:58 +0900

    fix : 로딩 문제 해결

--- TeamProject/Client/Private/MainApp.cpp
+++ TeamProject/Client/Private/MainApp.cpp
@@ -167,7 +167,7 @@ void CMainApp::Set_Levels()
 
 	LevelManager()->Set_LoadingLevel("Loading_Level");
 	m_pGameInstance->Notify_LevelSet(); 
-	m_pGameInstance->Get_LevelMgr()->Request_ChangeLevel("Scott_Level", false);
+	m_pGameInstance->Get_LevelMgr()->Request_ChangeLevel("MainCity_Level", true);
 } 
 
 CMainApp* CMainApp::Create()
