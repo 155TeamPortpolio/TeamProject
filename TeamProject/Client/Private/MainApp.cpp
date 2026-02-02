b48ffc076f43c442282b214570495be476537ea7
조혜연 <adencho1204@naver.com>
2026-02-02 22:20:39 +0900

    start level 변경

--- TeamProject/Client/Private/MainApp.cpp
+++ TeamProject/Client/Private/MainApp.cpp
@@ -184,7 +184,7 @@ void CMainApp::Set_Levels()
 
 	LevelManager()->Set_LoadingLevel("Loading_Level");
 	m_pGameInstance->Notify_LevelSet(); 
-	m_pGameInstance->Get_LevelMgr()->Request_ChangeLevel("Gacha_Level", true);
+	m_pGameInstance->Get_LevelMgr()->Request_ChangeLevel("Scott_Level", true);
 
 
 } 
