0e052a232dedfb965b47e3b02cdfccc81d0c5f46
조혜연 <adencho1204@naver.com>
2026-02-03 16:13:02 +0900

    feat: 무기회전

--- TeamProject/Client/Private/GachaResult.cpp
+++ TeamProject/Client/Private/GachaResult.cpp
@@ -4,6 +4,8 @@
 #include "StaticModel.h"
 #include "Material.h"
 
+#include "Helper_Func.h"
+
 CGachaResult::CGachaResult()
     :CGameObject()
 {
@@ -23,6 +25,9 @@ void CGachaResult::SetResult(string strModel, string strMaterial, _float4 vRot)
     pMaterial->Link_Material("Gacha_Level", strMaterial);
 
     m_pTransform->Set_Quaternion(_vector4(vRot));
+
+    m_vInitRot = vRot;
+    m_fRotElapsedTime = 0.f;
 }
 
 HRESULT CGachaResult::Initialize_Prototype()
@@ -57,6 +62,22 @@ void CGachaResult::Priority_Update(_float dt)
 
 void CGachaResult::Update(_float dt)
 {
+    if (m_fRotElapsedTime < m_fRotDuration)
+    {
+        m_fRotElapsedTime += dt;
+
+        _float fProgress = min(m_fRotElapsedTime / m_fRotDuration, 1.f);
+
+        _float fEasedProgress = Math::ApplyEase(EaseType::OutCubic, fProgress);
+
+        _float fYRotation = fEasedProgress * XM_2PI;
+
+        XMVECTOR vInitQuat = m_vInitRot;
+        XMVECTOR vYRotQuat = XMQuaternionRotationAxis(XMVectorSet(0, 1, 0, 0), fYRotation);
+        XMVECTOR vFinalQuat = XMQuaternionMultiply(vInitQuat, vYRotQuat);
+
+        m_pTransform->Set_Quaternion(vFinalQuat);
+    }
 }
 
 void CGachaResult::Late_Update(_float dt)
