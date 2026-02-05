9d611b99740f410e2d4eec0cf1ad28cf1d2434c8
조혜연 <adencho1204@naver.com>
2026-02-05 16:34:30 +0900

    refactor: character face shader #1162

--- TeamProject/Client/Private/Gacha_Level.cpp
+++ TeamProject/Client/Private/Gacha_Level.cpp
@@ -40,7 +40,7 @@ HRESULT CGacha_Level::Awake()
 	LIGHT_DESC lightDesc = {};
 	lightDesc.vLightPosition = _float4(-50.f, 50.f, 0.f, 1.f);
 	lightDesc.vLightDiffuse = _float4(1.f, 1.f, 1.f, 1.f);
-	lightDesc.vLightAmbient = _float4(0.5f, 0.5f, 0.5f, 1.f);
+	lightDesc.vLightAmbient = _float4(0.7f, 0.7f, 0.7f, 1.f);
 	lightDesc.vLightSpecular = _float4(0.f, 0.f, 0.f, 1.f);
 	lightDesc.fLightIntensity = 1.f;
 	pShadowCam->Get_Component<CLight>()->Set_Desc(lightDesc, LIGHT_TYPE::DIRECTIONAL);
