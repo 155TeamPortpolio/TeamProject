5d6c1a1510d9f8403d994fe0e78d037c718ec1b4
woo <leewooseok1114@gmail.com>
2026-02-06 17:33:44 +0900

    feat : 미야비 이펙트 추가 #1266

--- TeamProject/Client/Private/MiyabiState_ExAttack.cpp
+++ TeamProject/Client/Private/MiyabiState_ExAttack.cpp
@@ -132,6 +132,17 @@ void CMiyabiState_ExAttack_01::Update(CMiyabi* pOwner, _float dt)
     pOwner->Process_RootMotion(dt,
         ENUM(CMiyabi::ROOTMOTION_MASK::MOVE) |
         ENUM(CMiyabi::ROOTMOTION_MASK::QUATERNION));
+
+    Update_Effects(pOwner);
+}
+
+void CMiyabiState_ExAttack_01::Update_Effects(CMiyabi* pOwner)
+{
+    if (IsCrossAnimProgress(0.17f))
+    {
+        pOwner->Play_Effect("Miyabi_Ex0_Slash0", _vector3(0.f, 0.7f, 0.f), _quaternion(-0.21f, 0.71f, 0.64f, 0.2f));
+        pOwner->Play_Effect("Miyabi_Ex0_Sting0", _vector3(-6.6f, 0.8f, 2.5f), _quaternion(0.f, 0.f, 0.f, 1.f), false);
+    }
 }
 
 void CMiyabiState_ExAttack_02::Enter(CMiyabi* pOwner)
