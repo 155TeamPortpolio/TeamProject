af2cf7efe4328d70ded4401edc81c8057bf4b85a
boogiu <86179065+boogiu@users.noreply.github.com>
2026-02-07 21:14:02 +0900

    fix : 사운드 없을 시 터지는 문제 수정

--- TeamProject/Engine/Private/AudioSource.cpp
+++ TeamProject/Engine/Private/AudioSource.cpp
@@ -282,17 +282,19 @@ void CAudioSource::Play(const string& SoundKey)
 		return;
 
 	AUDIO_SLOT& slot = iter->second;
-
+	if (slot.pSound == nullptr)
+		return;
 	_float now = CGameInstance::GetInstance()->Get_TimeMgr()->Get_TotalTime("Audio_Timer");
 
 	if (now - slot.lastPlayTime < 0.05f)
 		return;
+	
 	slot.lastPlayTime = now;
 
 	AUDIO_PACKET packet{};
 	packet.ppChannelToUpdate = &slot.pChanel;
 	packet.pSound = slot.pSound;
-
+	
 	// 슬롯에서 설정한 값들 싹 복사
 	packet.isInfinite = slot.isInfinite;
 	packet.isPaused = slot.isPaused;
