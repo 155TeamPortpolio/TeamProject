950770df39492b51f2c8493bb10070bb291c4bcc
jehyun1108 <andixuan_1108@naver.com>
2026-02-10 15:16:37 +0900

    feat : Sound 추가작업 #1332

--- TeamProject/Client/Public/CorinState_ExAttack.h
+++ TeamProject/Client/Public/CorinState_ExAttack.h
@@ -62,7 +62,7 @@ class CCorinState_ExAttack_Explode : public IBaseState<CCorin>
 public:
     virtual void Enter(CCorin* pOwner) override;
     virtual void Update(CCorin* pOwner, _float dt) override;
-    virtual void Exit(CCorin* pOwner) override {};
+    virtual void Exit(CCorin* pOwner) override;
 
 public:
     static CCorinState_ExAttack_Explode* Create() { return new CCorinState_ExAttack_Explode(); }
