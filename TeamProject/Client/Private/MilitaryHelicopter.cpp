ae6a3074f561b606e91be7b40ef30ced9eaec77b
na4742 <126836684+na4742@users.noreply.github.com>
2026-02-13 15:30:12 +0900

    feat : 사운드 작업

--- TeamProject/Client/Private/MilitaryHelicopter.cpp
+++ TeamProject/Client/Private/MilitaryHelicopter.cpp
@@ -59,7 +59,11 @@ void CMilitaryHelicopter::Awake()
         .Apply();
 
     Get_Component<CAudioSource>()->SoundFolder("Scott_Level", "../Bin/Resources/Scott/AmbientActor/MilitaryHelicopter/Sound/");
-    Get_Component<CAudioSource>()->Slot("Helicopter.wav").Attribute3D(true).Loop(true).Play();
+    Get_Component<CAudioSource>()->Slot("Helicopter.wav")
+        .Attribute3D(true)
+        .Loop(true)
+        .Volume(0.3f)
+        .Play();
 }
 
 void CMilitaryHelicopter::Priority_Update(_float dt)
@@ -69,7 +73,8 @@ void CMilitaryHelicopter::Priority_Update(_float dt)
 void CMilitaryHelicopter::Update(_float dt)
 {
     Get_Component<CAnimator3D>()->Update_Animation(dt);
-    //Get_Component<CAudioSource>()->
+    Get_Component<CAudioSource>()->Set_AudioPos(
+        Get_Component<CAnimator3D>()->Get_BonePosition(CAnimator3D::BoneSpace::WORLD, "Ctr_Main"));
 }
 
 void CMilitaryHelicopter::Late_Update(_float dt)
