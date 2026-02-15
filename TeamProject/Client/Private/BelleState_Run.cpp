a74d8fe088cda7131b119ef8ba211c6d3a9279b6
조혜연 <adencho1204@naver.com>
2026-02-15 21:40:47 +0900

    feat: belle 사운드 수정 및 가챠 bgm 사운드 추가 #1432

--- TeamProject/Client/Private/BelleState_Run.cpp
+++ TeamProject/Client/Private/BelleState_Run.cpp
@@ -39,7 +39,7 @@ void CBelleState_Run::Update(CBelle* pOwner, _float dt)
 
     for (const auto& Event : pOwner->Get_Component<CAnimator3D>()->Get_EventBus())
     {
-        if (Event.Type != CLIP_EVENT_TYPE::NOTIFY) continue;
+        if (Event.Type != CLIP_EVENT_TYPE::SOUND) continue;
 
         if (Event.Tag.size() >= 4 &&
             Event.Tag.substr(Event.Tag.size() - 4) == ".wav")
