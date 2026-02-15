b2818bbac76ed42da0cce67001c4539e5160c662
조혜연 <adencho1204@naver.com>
2026-02-15 20:24:52 +0900

    fieldsystem 충돌해결

--- TeamProject/Client/Private/FieldSystem.cpp
+++ TeamProject/Client/Private/FieldSystem.cpp
@@ -22,9 +22,8 @@ CFieldSystem::CFieldSystem()
 	m_DayTime.StartFog = { _float4(0.95f, 0.75f, 0.8f, 1.0f),0.003f };
 	m_DayTime.StartCloud = { _float3(0.7f, 0.5f, 0.65f), _float3(0.95f, 0.7f, 0.75f) };
 
-	//m_pBGM = CAudioSource::Create();
-	//D:\Study\TeamProject\TeamProject\Client\Bin\Resources\MainCity\Sound\NPC
-	//m_pBGM->SoundFolder(G_GlobalLevelKey, "../Bin/Resources/Global/BGM");
+	m_pBGM = CAudioSource::Create();
+	m_pBGM->SoundFolder(G_GlobalLevelKey, "../Bin/Resources/Global/BGM");
 }
 
 void CFieldSystem::Update()
