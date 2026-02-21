b866100abab46d27a9b3f438d007b95bf8037d59
tballoon23 <asbssu18@gmail.com>
2026-02-21 17:10:49 +0900

    feat : Parry, Evade Sound

--- TeamProject/Client/Private/JaneDoeState_Evade.cpp
+++ TeamProject/Client/Private/JaneDoeState_Evade.cpp
@@ -8,6 +8,7 @@
 #include "JaneDoe.h"
 
 #include "CharacterController.h"
+#include "AudioSource.h"
 
 #include "JaneDoeState_Dash.h"
 #include "JaneDoeState_Backstep.h"
@@ -63,6 +64,9 @@ void CJaneDoeState_Evade::Update(CJaneDoe* pOwner, _float dt)
                 desc.eAction = TUTORIAL_ACTION::DODGE;
                 EventSystem()->Broadcast<TUTORIAL_ACTION_DESC>(desc);
             }
+            pOwner->Get_Component<CAudioSource>()->Slot("PerfectEvade")
+                .Attribute3D(true)
+                .PlayUnique();
             pOwner->Play_Effect("Evade", _vector3(0.f, 0.f, 0.f), _quaternion(0.f, 0.f, 0.f, 1.f), false);
             m_pSubStateMachine->Set_Bool("Extreme", true);
         }
