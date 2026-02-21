112760476aa4d5ba039e17fa3f30fabf900c28a5
조혜연 <adencho1204@naver.com>
2026-02-21 21:22:15 +0900

    feat: ui fade global

--- TeamProject/Client/Private/LoadingLevel.cpp
+++ TeamProject/Client/Private/LoadingLevel.cpp
@@ -55,6 +55,7 @@ void CLoadingLevel::Update()
 	pRcsMgr->GetPreloadProgress(done, total);
 	if (ResourceManager()->isLoadComplete() &&(m_leastTime>1.f))
 	{
+		UIDirector()->FadeOut_Screen(1.f);
 		LevelManager()->Notify_LoadComplete();
 	}
 
--- TeamProject/Client/Private/MainCity_Level.cpp
+++ TeamProject/Client/Private/MainCity_Level.cpp
@@ -72,6 +72,7 @@ HRESULT CMainCity_Level::Awake()
 		RuntimeBucket().Int64.Set(PersistScope::SaveSlot, "MainCity_Level", Version);
 	}
 
+	UIDirector()->FadeIn_Screen(1.f);
 	return S_OK;
 }
 
