908e33102b3724451bef08ce4f48b473ee95dd1d
조혜연 <adencho1204@naver.com>
2026-02-21 10:56:38 +0900

    feat: portal 마지막 세팅 #1548

--- TeamProject/Client/Private/ZeroPortal.cpp
+++ TeamProject/Client/Private/ZeroPortal.cpp
@@ -232,7 +232,7 @@ void CZeroPortal::NoiseSequence()
 	auto pPost = RenderSystem()->GetPostRenderer();
 	pPost->GetCommand<CNoiseCommand>()
 		->SetTexture(ResourceManager()->Load_Texture(G_GlobalLevelKey, "VX_Noise_XL_09.png"))
-		->SetDuration(0.15f)
+		->SetDuration(0.25f)
 		->SetNum(4)
 		->SetEnable(true);
 }
@@ -297,7 +297,7 @@ void CZeroPortal::PortalEffectFlowSetting()
 	m_PortalFlow.AddWait(sequenceId, 0.1);
 
 	m_PortalFlow.AddOnce(sequenceId, [this, pPost]() {
-		UIDirector()->FadeOut_Screen(0.05f);
+		UIDirector()->FadeOut_Screen(0.15f);
 		});
 
 	m_PortalFlow.EndSequence(sequenceId);
