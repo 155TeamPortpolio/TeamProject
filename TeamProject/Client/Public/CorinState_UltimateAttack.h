629a4146c1af99ff92eb4d2417edc8d36fa6501c
tballoon23 <asbssu18@gmail.com>
2026-02-22 14:45:23 +0900

    fix : 회피판정증가

--- TeamProject/Client/Public/CorinState_UltimateAttack.h
+++ TeamProject/Client/Public/CorinState_UltimateAttack.h
@@ -34,7 +34,7 @@ class CCorinState_UltimateAttack_Loop : public IBaseState<CCorin>
 public:
     virtual void Enter(CCorin* pOwner) override;
     virtual void Update(CCorin* pOwner, _float dt) override;
-    virtual void Exit(CCorin* pOwner) override {}
+    virtual void Exit(CCorin* pOwner) override;
 
 public:
     static CCorinState_UltimateAttack_Loop* Create() { return new CCorinState_UltimateAttack_Loop(); }
